4129800cc2eb8a9231f8453149fa8eda871a848a
Arthur Saito <arthur.saito@nuria.com.br>
2024-07-19 23:39:48 -0300

    + building params to threadPool

--- AS.AsyncAwait.Kernel/SThreadPool.cs
+++ AS.AsyncAwait.Kernel/SThreadPool.cs
@@ -1,6 +1,23 @@
-﻿namespace AS.AsyncAwait.Kernel;
+﻿using System.Collections.Concurrent;
+
+namespace AS.AsyncAwait.Kernel;
 
 public static class SThreadPool
 {
-    
+    private static readonly BlockingCollection<Action> _tasks = new();
+    public static void QueueTask(Action task) => _tasks.Add(task);
+
+    static SThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    
+                }
+            }){IsBackground = true}.Start();
+        }
+    }
 }
