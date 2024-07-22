f34bbbccd38edc00410759aa458e3e9cf254607f
Arthur Saito <arthur.saito@nuria.com.br>
2024-07-22 18:51:39 -0300

    +Task Api

---
+++ AS.AsyncAwait.Kernel/STask.cs
@@ -0,0 +1,25 @@
+﻿namespace AS.AsyncAwait.Kernel;
+
+public class STask
+{
+    private bool _completed;
+    private Exception? _exception;
+    private Action? _continuation;
+    private ExecutionContext? _context;
+
+    public bool IsCompleted
+    {
+        get
+        {
+            lock (this)
+            {
+                return _completed;
+            }
+        }
+    }
+
+    public void SetResult(){}
+    public void SetException(Exception exception){}
+    public void Wait(){}
+    public void ContinueWith(Action continuation){}
+}
